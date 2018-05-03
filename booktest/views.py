581f76275ba5ee33340dbbd676940bc3aebf5736
bwylove <bwylove@126.com>
2018-05-03 22:19:14 +0800

    入门

--- booktest/views.py
+++ booktest/views.py
@@ -7,5 +7,6 @@ from django.template import RequestContext,loader
 # Create your views here.
 
 def index(request):
-    temp=loader.get_template('booktest/index.html')
-    return HttpResponse(temp.render())
+    # temp=loader.get_template('booktest/index.html')
+    # return HttpResponse(temp.render())
+    return render(request,'booktest/index.html')
